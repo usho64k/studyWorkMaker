f4ecf75927dd86ff218c096e7553c15bc98293a1
usho64k <usuallybreadspace@gmail.com>
2020-01-27 12:31:01 +0900

    6666

--- main.pl
+++ main.pl
@@ -2,13 +2,20 @@
 use strict;
 use warnings;
 
+my @strary;
+foreach my $a(<test.txt>){
+@strary=(@strary,$a);
+}
+
+
+
 #標準入力
 my $line = <STDIN>;
 chomp($line);
 print "a : $line \n";
 
 #基本的にはこの使い方をする
-my $targetstr = "卑弥呼邪馬台国の女王";
+my $targetstr = "卑弥=呼邪馬台国の女王";
 my $isContainsEqual = ($targetstr =~ /=/);
 
 if($isContainsEqual != 0)
