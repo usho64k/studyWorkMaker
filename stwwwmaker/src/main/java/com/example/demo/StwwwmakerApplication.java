e603c2254e1f2694db4cbb40f0f917fd38f1776f
usho64k <usuallybreadspace@gmail.com>
2020-06-25 10:53:29 +0900

    JavaSts included

---
+++ stwwwmaker/src/main/java/com/example/demo/StwwwmakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StwwwmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StwwwmakerApplication.class, args);
+	}
+
+}
