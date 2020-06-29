62ff4e3491d5943bdcf4917819c814b06ff69edd
usho64k <usuallybreadspace@gmail.com>
2020-06-29 15:31:31 +0900

    とりあえず準備すべきhtmlはそろえた

--- sts/stwwmaker/src/main/java/com/example/demo/StwwMain.java
+++ sts/stwwmaker/src/main/java/com/example/demo/StwwMain.java
@@ -3,11 +3,38 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StwwMain {
 	@RequestMapping(value="/")
-	public String index(Model model) {
-		return "index1";
+	public ModelAndView index(Model model) {
+		ModelAndView m = new ModelAndView();
+		m.setViewName("index1.html");
+		return m;
+	}
+	
+	@RequestMapping(value="/qapush")
+	public ModelAndView qapush(@ModelAttribute String qakjfjk) {
+		ModelAndView m = new ModelAndView();
+		m.setViewName("index_qapush.html");
+		return m;
+	}
+	
+	@RequestMapping(value="/qanda")
+	public ModelAndView qanda(@ModelAttribute qaObject qaList) {
+		ModelAndView m = new ModelAndView();
+		m.setViewName("index_qa.html");
+		return m;
+	}
+
+
+	public static class qaObject{
+		private String filename;
+		
+		public String getFilename() { return filename;	}
+		public void setFilename(String str) {	filename = str; }
 	}
 }
+
