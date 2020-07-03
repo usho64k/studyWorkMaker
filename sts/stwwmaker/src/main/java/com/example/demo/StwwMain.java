81f318ef729668fa769310b787dacaa06e835c52
usho64k <usuallybreadspace@gmail.com>
2020-07-03 16:11:36 +0900

    不要なコードを削除、必要なコメントを追記、少し手直し

--- sts/stwwmaker/src/main/java/com/example/demo/StwwMain.java
+++ sts/stwwmaker/src/main/java/com/example/demo/StwwMain.java
@@ -5,37 +5,29 @@ import org.springframework.stereotype.Service;
 
 //Springframework.web.bind系 汎用クラス
 import org.springframework.ui.Model;							//引数
-import org.springframework.web.bind.annotation.RequestMapping;	//HTMLアクセス関数登録
-import org.springframework.web.bind.annotation.RequestParam;	//URL引数をうけとるやつ
-import org.springframework.web.bind.annotation.ModelAttribute;	//HTMLに渡すThymeleaf機能クラス
-import org.springframework.web.bind.annotation.ResponseBody;	//これはしらない
-import org.springframework.web.bind.annotation.PostMapping;		//HTTPリクエストのPOSTアクセス？
-import org.springframework.web.bind.annotation.GetMapping;		//HTTPリクエストのGETアクセス？
-import org.springframework.web.bind.annotation.ExceptionHandler;	//例外ハンドラ
-import org.springframework.web.bind.annotation.PathVariable;	//なんだろう？
+import org.springframework.web.bind.annotation.RequestMapping;	//HTMLアクセス関数登録するためのアノテーション
+import org.springframework.web.bind.annotation.RequestParam;	//URL引数をうけとるためのアノテーション
+import org.springframework.web.bind.annotation.ModelAttribute;	//HTMLに渡すThymeleaf機能クラスを使うアノテーション
+import org.springframework.web.bind.annotation.ResponseBody;	//POSTで要求されるBody側Stringを返すことを明示するアノテーション
+import org.springframework.web.bind.annotation.PostMapping;		//HTTPリクエストのPOSTアクセスアノテーション
+import org.springframework.web.bind.annotation.GetMapping;		//HTTPリクエストのGETアクセスアノテーション
 import org.springframework.web.multipart.MultipartFile;			//CSV処理に必要
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;	//使っていない？
 import org.springframework.web.servlet.ModelAndView;			//HTMLViewそのもの
 import org.springframework.beans.factory.annotation.Autowired;	//なにこれ？
 
+//配列処理用
 import java.util.ArrayList;
 import java.util.List;
-import javax.servlet.http.HttpServletRequest;					//POSTの中身受け取るやつ
 
 //CSV処理に必要なやつ
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.Reader;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import java.util.stream.Collectors;
+import java.io.InputStream;										//最初に処理するStream
+import java.io.Reader;											//Reader
+import java.io.InputStreamReader;								//StreamReader(初期化子)
+import java.io.BufferedReader;									//StreamからBufferにする
 
+//コンソール画面へのデバッグ用
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -44,18 +36,13 @@ public class StwwMain {
 	//MySQLへのRepositoryInterface
 	@Autowired
 	private qaListRepository qalistRepository2;
-	//CSVへのService Interface
-	//private final StorageService storageService;
 	
-	//@Autowired
-	//public StwwMain(StorageService storageService) {
-	//	this.storageService = storageService;
-	//}
-	
-	@Controller
+	//テスト用(一応サービスで定義しておいてもいいはず)
+	@Service
 	@RequestMapping(value="/demo")
 	public class Controller2
 	{
+		//テスト用
 		@Autowired
 		private qaListRepository qalistRepository;
 		@PostMapping(path="/add")
@@ -75,35 +62,44 @@ public class StwwMain {
 		
 	}
 	
-	
+	//メインページ
 	@RequestMapping(value="/stwwtest")
-	public ModelAndView index(Model model) {
+	public ModelAndView index(Model model) 
+	{
 		ModelAndView m = new ModelAndView();
 		m.setViewName("index1.html");
 		m.addObject("qaListRow",new qaListRow());
 		return m;
 	}
 	
+	//問題・解答データの挿入
 	@PostMapping(value="/qaPush")
-	public ModelAndView qapush(@RequestParam("qaCsv") MultipartFile file,RedirectAttributes red,Model model) {
+	public ModelAndView qapush(@RequestParam("qaCsv") MultipartFile file,RedirectAttributes red,Model model) 
+	{
 		ModelAndView m = new ModelAndView();
 		ArrayList<qaListRow> qalist = new ArrayList<qaListRow>();	//HTMl表示用のリスト
+		qalist.add(new qaListRow("Question","Answer"));
 		//貰ったCSVを展開
 		red.addFlashAttribute("message","You successfully uploaded "+file.getOriginalFilename()+"!");
 
-		if(file.isEmpty()) {
+		if(file.isEmpty() || file == null) 
+		{
+			//空文字、もしくはNullだった場合
 			final Logger log = 	LoggerFactory.getLogger(StwwmakerApplication.class);
 			log.info("brank character");
 		}
 		else
 		{
+			//ログ表示(デバッグ用コンソール画面)
 			final Logger log = 	LoggerFactory.getLogger(StwwmakerApplication.class);
-			log.info(file.toString());
+			log.info(file.getOriginalFilename());
 			
 			//CSV展開処理
 			List<String> lines = new ArrayList<String>();
 			String line = null;
-			try {
+			try 
+			{
+				//ファイル内を確認
 				InputStream stream = file.getInputStream();
 				Reader reader = new InputStreamReader(stream);
 				BufferedReader buf = new BufferedReader(reader);
@@ -115,25 +111,22 @@ public class StwwMain {
 			}
 			catch(IOException e) 
 			{
+				//読み込みできなかった場合
 				log.info("Can't read contents.");
 			}
-			for(String s : lines){
+			//読み込みできた部分まで出力
+			for(String s : lines)
+			{
+				//"="で前後に分ける
 				String[] elem = s.split("=",2);
+				//SQLにアクセスする
 				qalistRepository2.save(new qaListRow(elem[0],elem[1]));
+				//ログ出力(ページに表示)
 				qalist.add(new qaListRow(elem[0],elem[1]));
 			}
-			
-			
 		}
-		//SQLにアクセスする
-		qalistRepository2.save(new qaListRow("TaQ","ans2"));
 
-		//tableに表示させたい要素はqaStrsにぶっこむ(以下ぶっこみサンプル)
-		qalist.add(new qaListRow("How's the weather?","It's sunny."));
-		qalist.add(new qaListRow("How's whether?","I'm fine."));
-		qalist.add(new qaListRow("How's the feather?","It's so tired."));
-		qalist.add(new qaListRow("How's the teacher?","It's fun."));
-		qalist.add(new qaListRow("How's the measure?","It's 72cm."));
+		//qaStrsに反映
 		model.addAttribute("qaStrs",qalist);
 		
 		//転送結果を表示
@@ -154,7 +147,5 @@ public class StwwMain {
 		
 		public String getFilename() { return filename;	}
 		public void setFilename(String str) {	filename = str; }
-	}
-	
+	}	
 }
-
